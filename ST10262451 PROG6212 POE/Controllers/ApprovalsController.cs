737cb794e11083059733b48dc7a985a2b5b90d38
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2024-10-18 17:04:24 +0200

    Added ApprovalsController.cs

---
+++ ST10262451 PROG6212 POE/Controllers/ApprovalsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProject.Controllers
+{
+    public class ApprovalsController : Controller
+    {
+        private readonly IClaimService _claimService;
+
+        public ApprovalsController(IClaimService claimService)
+        {
+            _claimService = claimService;
+        }
+
+        // GET: Approvals/Index (View pending claims)
+        public async Task<IActionResult> Index()
+        {
+            var pendingClaims = await _claimService.GetPendingClaimsAsync();
+            return View(pendingClaims);
+        }
+
+        // POST: Approvals/Approve
+        [HttpPost]
+        public async Task<IActionResult> Approve(int id)
+        {
+            await _claimService.ApproveClaimAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Approvals/Reject
+        [HttpPost]
+        public async Task<IActionResult> Reject(int id)
+        {
+            await _claimService.RejectClaimAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
