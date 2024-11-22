d796b73b89d87d5727d377af51a1d77c51e39fe8
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2024-11-22 22:24:12 +0200

    Updated DbContext

---
+++ ST10262451 PROG6212 POE/Controllers/YourDbContext.cs
@@ -0,0 +1,28 @@
+﻿public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    { }
+
+    public DbSet<Claim> Claims { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+}
+
+public class ApplicationUser
+{
+}
+
+public class DbSet<T>
+{
+}
+
+public class DbContext
+{
+    public DbContext(DbContextOptions<ApplicationDbContext> options)
+    {
+    }
+}
+
+public class DbContextOptions<T>
+{
+}
